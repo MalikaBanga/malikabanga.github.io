597982b57c9c93872b1d83a60a2e029f904f4949
Malika Banga <Malika Banga>
2017-02-19 18:15:44 +0530

    made the home page responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,32 @@
 (function($) {
-	var $scrollTopElem = $('#scroll-to-top');
-	$(window).scroll(function(){
+    var $scrollTopElem = $('#scroll-to-top');
+    $(window).scroll(function() {
         if ($(this).scrollTop() > 200) {
-             $scrollTopElem.fadeIn();
+            $scrollTopElem.fadeIn();
         } else {
-             $scrollTopElem.fadeOut();
+            $scrollTopElem.fadeOut();
         }
     });
     $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+    $('#menu').click(function() {
+        $('#site-navigation').slideToggle();
+    });
+
+    $('body').on('click', function(e) {
+        if (!($(e.target).hasClass('navigation') ||
+                $(e.target).hasClass('mobile-menu') || $(e.target).parents().hasClass('navigation') || $(e.target).parents().hasClass('mobile-menu'))) {
+            $('#site-navigation').slideUp();
+        }
+    });
 }(jQuery));
